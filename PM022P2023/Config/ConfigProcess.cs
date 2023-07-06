df4888124409ac43951221be0ec871f0cebfae47
CAAndinoS <124225358+CAAndinoS@users.noreply.github.com>
2023-07-05 20:03:03 -0600

    Agregar archivos de proyecto.

---
+++ PM022P2023/Config/ConfigProcess.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM022P2023.Config
+{
+    public static class ConfigProcess
+    {
+        // Web Api http
+        public static String ipaddress = "192.168.1.3";
+        public static string webapi = "PM023PCRUD";
+
+        //Rauting METHOD POST
+        public static string postRaute = "AlumnCreate.php";
+        //Rauting METHOD GET
+        public static string getRaute = "AlumnGetList.php";
+
+        //Format URL
+        public static string formaturl = "http://{0}/{1}/{2}";
+
+        // URL Endpoint
+        public static string ApiGET = string.Format(formaturl, ipaddress, webapi, getRaute);
+        public static string ApiPOST = string.Format(formaturl, ipaddress, webapi, postRaute);
+    }
+}
---
+++ PM022P2023/Views/PageCreateAlum.xaml.cs
@@ -0,0 +1,71 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PM022P2023.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageCreateAlum : ContentPage
+    {
+        Plugin.Media.Abstractions.MediaFile photo = null;
+        public PageCreateAlum()
+        {
+            InitializeComponent();
+        }
+        public String Getimage64()
+        {
+            if (photo != null)
+            {
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    Stream stream = photo.GetStream();
+                    stream.CopyTo(memory);
+                    byte[] fotobyte = memory.ToArray();
+
+                    String Base64 = Convert.ToBase64String(fotobyte);
+
+                    return Base64;
+                }
+            }
+
+            return null;
+        }
+        private async void btnfoto_Clicked(object sender, EventArgs e)
+        {
+            photo = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                Directory = "MYAPP",
+                Name = "Foto.jpg",
+                SaveToAlbum = true
+            });
+
+            if (photo != null)
+            {
+                foto.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+            }
+        }
+
+        private async void btnguardar_Clicked(object sender, EventArgs e)
+        {
+            var alum = new Models.Alumnos{
+                nombres = nombres.Text,
+                apellidos = apellidos.Text,
+                direccion = direccion.Text,
+                telefono = Convert.ToInt32(telefono.Text),
+                foto = Getimage64()
+            };
+            Models.Msg resultado = await Controllers.AlumController.CreateAlum(alum);
+            if (resultado != null)
+            {
+                await DisplayAlert("aviso", resultado.message.ToString(), "OK");
+            }
+        }
+    }
+}
